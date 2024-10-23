3f804a4920516e79630013a1ea3f2abad8795c3a
Roger Chau <54104821+roger-chau@users.noreply.github.com>
2024-10-23 17:33:23 -0600

    Fixed hasCity

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -54,7 +54,12 @@ public class CustomList extends ArrayAdapter<City> {
     }
 
     public int hasCity(City city) {
-        return 0;
+        if (cities.contains(city)) {
+            return 1;
+        }
+        else {
+            return 0;
+        }
     }
 
 }
